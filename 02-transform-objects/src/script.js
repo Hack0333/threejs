d40e9c85fcfa191c25a86a4bd0ae595d9acf1390
Deepak Kumar Sharma <absharma0333@gmail.com>
2024-08-20 15:36:59 +0530

    Transformation

--- 02-transform-objects/src/script.js
+++ 02-transform-objects/src/script.js
@@ -9,16 +9,55 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
 
+// mesh.position.x = 1;
+// mesh.position.y = 1;
+// mesh.position.z =-1;
+// set mesh length to 1
+// mesh.position.normalize();
+
+// console.log(mesh.position.length());
+// console.log(mesh.position.distanceTo(new THREE.Vector3(1,2,1)));
+
+// mesh.scale.set(0.5,1,.9);
+// mesh.rotateX(35);
+// mesh.rotation.reorder("ZYX")
+// scene.add(mesh);
+
+const group = new THREE.Group();
+scene.add(group)
+
+const box1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+)
+group.add(box1);
+
+const box2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+)
+group.add(box2);
+
+const box3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshBasicMaterial({ color: "white" })
+)
+group.add(box3);
+
+box1.position.x =-2;
+box2.position.x = 2;
+
+// moving all the box together
+group.position.set(0.5,2,-1)
 /**
  * Sizes
  */
 const sizes = {
-  width: 800,
+  width: 700,
   height: 600,
 };
 
@@ -26,7 +65,14 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 4;
+// camera.position.set(1,1,3);
+// camera.lookAt(mesh.position)
+// console.log(mesh.position.distanceTo(camera.position));
+
+// const axesHelper = new THREE.AxesHelper()
+// red = x , green = y , blue = z
+// scene.add(axesHelper);
 scene.add(camera);
 
 /**
@@ -37,3 +83,4 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
