7b76c87f9e83fef9ec55375b1302d51adb155584
Deepak Kumar Sharma <absharma0333@gmail.com>
2024-08-23 18:39:52 +0530

    geometry

--- 06-fullscreen-and-resizing/src/script.js
+++ 06-fullscreen-and-resizing/src/script.js
@@ -47,6 +47,7 @@ window.addEventListener("resize",()=>{
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
   camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
   renderer.setSize(sizes.width, sizes.height);
 })
 // window.addEventListener("dblclick",(e)=>{
